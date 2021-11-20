47e3f57720d8e0900f86bf1755df277ec6c3459c
Fallenhacker29 <56514528+Fallenhacker29@users.noreply.github.com>
2021-11-20 13:32:33 +0300

    First commit Monastyrskiy lesson_001

---
+++ lesson_001/Monastyrskiy/code.py
@@ -0,0 +1,18 @@
+import os
+import io
+os.chdir('red_hat')
+def f1(what_to,by_what):
+    buff = []
+    for roots, dirs,files in os.walk("."):
+        for path in files:
+            with open(path, encoding='utf-8') as f:
+                for line in f:
+                    res = line.replace(what_to,by_what,1)
+                    print(res)
+                    buff.append(res)
+            with open(path, encoding='utf-8',mode="w") as R_file:
+                R_file.writelines(buff)
+f1('Чёрная Курточка','Красная Шапочка')
+
+
+
