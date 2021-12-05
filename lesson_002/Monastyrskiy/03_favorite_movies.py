f4d3b7c902f9fbd40efbad0db701cedb588db2d2
Fallenhacker29 <56514528+Fallenhacker29@users.noreply.github.com>
2021-12-05 12:30:57 +0300

    Homework commit lesson_002 Monastyrskiy

---
+++ lesson_002/Monastyrskiy/03_favorite_movies.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Есть строка с перечислением фильмов
+
+my_favorite_movies = 'Терминатор, Пятый элемент, Аватар, Чужие, Назад в будущее'
+
+# Выведите на консоль с помощью индексации строки, последовательно:
+#   первый фильм
+#   последний
+#   второй
+#   второй с конца
+
+# Переопределять my_favorite_movies и использовать .split() нельзя.
+# Запятая не должна выводиться.
+buffer = ""
+list_of_fav = []
+for char in my_favorite_movies:
+    if char == ",":
+        list_of_fav.append(buffer)
+        buffer = ""
+    else:
+        buffer += char
+list_of_fav.append(buffer)
+queue = [0,-1,1,-2]
+for i in queue:
+    print(list_of_fav[i].strip())
+
+
