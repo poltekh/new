0a8a1110da6592269d9f47761411920d2653ba3e
Zorinalexew <zorin2806@gmail.com>
2021-12-14 11:42:59 +0300

    2nd commit

--- 003/Zorin/01_days_in_month.py
+++ 003/Zorin/01_days_in_month.py
@@ -8,7 +8,18 @@
 
 # Номер месяца получать от пользователя следующим образом
 user_input = input("Введите, пожалуйста, номер месяца: ")
-month = int(user_input)
+try:
+    month = int(user_input)
+except ValueError as e:
+    print("Ошибка: нужно ввести число")
+    exit (1)
 print('Вы ввели', month)
-
-
+if month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
+    print("Количество дней: ", 31)
+elif month == 4 or month == 6 or month == 9 or month == 11:
+    print("Количество дней: ", 30)
+elif month == 2:
+    print("Количество дней: ", 28)
+else:
+    print("Месяца с таким номером не существует, введите ещё раз")
+exit(2)
