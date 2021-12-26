63e3d9818574490a4b725428541881feba56596e
Илья <chubov.ilya@gmail.com>
2021-12-26 19:29:58 +0300

    Chubov Ilya 22

--- 003/Чубов/07_wall.py
+++ 003/Чубов/07_wall.py
@@ -1,25 +1,30 @@
 # -*- coding: utf-8 -*-
 
-# (определение функций)
-
+# (цикл for)
 import simple_draw as sd
-# Написать функцию отрисовки смайлика в произвольной точке экрана
-# Форма рожицы-смайлика на ваше усмотрение
-# Параметры функции: кордината X, координата Y, цвет.
-# Вывести 10 смайликов в произвольных точках экрана.
-sd.resolution = (1200,600)
 
+# Нарисовать стену из кирпичей. Размер кирпича - 100х50
+# Использовать вложенные циклы for
 
-def smile(x, y, color):
-    sd.circle(sd.Point (x,y), 30, color, 1)
-    sd.line(sd.Point(x - 10, y + 15), sd.Point(x - 10, y - 5),color, 1)
-    sd.line(sd.Point(x + 10, y + 15), sd.Point(x + 10, y - 5),color,1)
-    sd.line(sd.Point(x - 15, y - 15), sd.Point(x + 15, y - 15), color, 1)
+sd.resolution = (1000, 500)
+start_x = 0
+start_y = 0
+width = 100
+height = 50
+row_brick_number = 10
+row_number = 10
+brick_offset = int(0.5 * width)
 
+for current_row in range(row_number):
+    offset = 50 if current_row % 2 == 0 else 0
 
-for i in range(10):
-    x = sd.randint(30, 1100)
-    y = sd.randint(30, 570)
-    color = sd.random_color()
-    smile(x , y , color)
+    for current_brick in range(row_brick_number):
+        left_bottom_x = start_x + offset + width * current_brick
+        left_bottom_y = start_y + height * current_row
+        left_bottom = sd.get_point(left_bottom_x, left_bottom_y)
+        right_top_x = left_bottom_x + width
+        right_top_y = left_bottom_y + height
+        right_top = sd.get_point(right_top_x, right_top_y)
+        sd.rectangle(left_bottom, right_top, sd.COLOR_YELLOW, width=3)
+
 sd.pause()
