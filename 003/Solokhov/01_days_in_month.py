4cf24b21b5d3749bb2aed7a669b5efefb2783506
HP <densolo>
2021-12-26 23:14:48 +0300

    Denis

--- 003/Solokhov/01_days_in_month.py
+++ 003/Solokhov/01_days_in_month.py
@@ -7,18 +7,16 @@
 # Если номер месяца некорректен - сообщить об этом
 
 # Номер месяца получать от пользователя следующим образом
-user_input = input("Введите, пожалуйста, номер месяца: ")
-month = int(user_input)
-print('Вы ввели', month)
+
 
 user_input = input("Введите, пожалуйста, номер месяца: ")
 month = int(user_input)
 print('Вы ввели', month)
 if month in [1,3,5,7,8,10,12]:
-    print( '31')
+    print('31')
 elif month == 2:
-    print( '28')
+    print('28')
 elif month > 12:
-    print( 'Incorrect')
+    print('Неправильно')
 else:
-    print( '30' )
+    print('30')
